b05019f933aa7487797267ce9627257f4d851f07
evgenyman <54987777+evgenyman@users.noreply.github.com>
2021-10-16 21:04:19 +0300

    Update Main.java

--- task_2.2/Main.java
+++ task_2.2/Main.java
@@ -7,6 +7,11 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
-
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.print(j*i+" ");
+            }
+            System.out.println(" ");
+        }
     }
 }
