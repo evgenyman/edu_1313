7647523ea3150a80bc8b3cd11f93688f241f7b02
evgenyman <54987777+evgenyman@users.noreply.github.com>
2021-10-18 22:56:58 +0300

    Update Main.java

--- task_3.3/Main.java
+++ task_3.3/Main.java
@@ -1,1 +1,24 @@
+/*
+Задание: Сделайте функцию, которая параметрами принимает 2 числа. Если эти числа равны - пусть функция вернет true, а если не равны - false.
+*/
 
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+        Scanner vvodChisla = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        int a = vvodChisla.nextInt();
+        System.out.print("Введите второе число: ");
+        int b = vvodChisla.nextInt();
+
+        System.out.println("Результат сравнения двух чисел: "+ravenstvo(a,b));
+    }
+
+    public static boolean ravenstvo(int x, int y) {  // здесь важно использовать 'boolean' для того, чтобы функция Могла вернуть 'true' или 'false' (не использовать 'int' или 'void')
+        if (x==y) return true;
+        else return false;
+    }
+}
