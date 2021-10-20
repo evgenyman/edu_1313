ed91aa1d6fa80a3337480a8a0361c8972ec7251e
evgenyman <54987777+evgenyman@users.noreply.github.com>
2021-10-20 19:49:30 +0300

    Update Main.java

--- task_4.1/Main.java
+++ task_4.1/Main.java
@@ -1,1 +1,28 @@
+/*
+Помогите коту обрести имя с помощью метода setName.
+Требования:
+•	Класс Cat должен содержать только одну переменную name.
+•	Переменная name должна иметь модификатор доступа private и тип String.
+•	Метод setName класса Cat должен устанавливать значение переменной private String name равным переданному параметру String name.
+*/
 
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.setName("Жужик");
+        // System.out.println(cat.name);
+    }
+}
+
+class Cat {
+    private String name = "безымянный кот";
+
+    public void setName(String name) {
+        //напишите тут ваш код
+        System.out.println("Текущее имя - "+this.name);
+        this.name = name;
+        System.out.println("Имя стало - "+this.name);
+
+    }
+}
