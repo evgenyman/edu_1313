9afe06bb948eeb11a773fb90c3ed14b9eac7bcd1
evgenyman <54987777+evgenyman@users.noreply.github.com>
2021-10-21 19:52:48 +0300

    Create Main.java

---
+++ task_4.4/Main.java
@@ -0,0 +1,61 @@
+/*
+Создай классы Dog, Cat, Mouse.
+Добавь по три поля в каждый класс, на твой выбор.
+Создай объекты для героев мультика Том и Джерри.
+Так много, как только вспомнишь.
+Пример:
+Mouse jerryMouse = new Mouse("Jerry", 12 , 5),
+где 12 - высота в см,
+5 - длина хвоста в см.
+Требования:
+•	Создай класс Dog.
+•	Создай класс Cat.
+•	В классе Dog должно быть три переменные.
+•	В классе Cat должно быть три переменные.
+•	Должен быть создан хотя бы один объект типа Mouse.
+•	Должен быть создан хотя бы один объект типа Dog.
+•	Должен быть создан хотя бы один объект типа Cat.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        Mouse jerryMouse = new Mouse("Jerry", 12, 5);
+        Cat jerryCat = new Cat ("Tom", 40,10);
+        Dog jerryDog = new Dog ("Bob",50,15);
+
+        //напишите тут ваш код
+    }
+}
+class Mouse {
+    String name;
+    int height;
+    int tail;
+
+    public Mouse(String name, int height, int tail) {
+        this.name = name;
+        this.height = height;
+        this.tail = tail;
+    }
+}
+class Cat {
+    String name;
+    int height;
+    int tail;
+
+    public Cat(String name, int height, int tail) {
+        this.name = name;
+        this.height = height;
+        this.tail = tail;
+    }
+}
+class Dog {
+    String name;
+    int height;
+    int tail;
+
+    public Dog(String name, int height, int tail) {
+        this.name = name;
+        this.height = height;
+        this.tail = tail;
+    }
+}
